fdb55b2b813baaddfaa397b32d5dac015ca96e50
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2023-01-23 15:35:22 -0300

    start on posts page

---
+++ 02_ignews/src/pages/posts/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+    return (
+        <>
+        <Head>
+            <title>Posts | Ignews</title>
+        </Head>
+        </>
+    );
+}
