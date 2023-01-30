d74b6e27d21191a4cd2b4e16eecef6fd356f2a93
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2023-01-30 08:04:59 -0300

    start fundamentals

---
+++ 01-fundamentals-reactjs/src/App.jsx
@@ -0,0 +1,17 @@
+import { Post } from "./Post"
+
+export function App() {
+
+  return (
+    <>
+      <Post
+        author="Rafael Pablo"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur dicta voluptate sapiente id vel beatae facere tempore natus architecto, eaque praesentium libero temporibus, quasi nisi laudantium ducimus, voluptas esse."
+      />
+      <Post
+        author="Kelly Eduarda"
+        content="Teste de post."
+      />
+    </>
+  )
+  }
