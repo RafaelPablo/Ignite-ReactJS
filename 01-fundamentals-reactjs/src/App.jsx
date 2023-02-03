654031e6832068a695949fc6d4b357d4c9622f8b
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2023-02-03 16:37:34 -0300

    finishing header and start of sidebar

--- 01-fundamentals-reactjs/src/App.jsx
+++ 01-fundamentals-reactjs/src/App.jsx
@@ -1,20 +1,30 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
 
   return (
     <>
-    <Header />
-      <Post
-        author="Rafael Pablo"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur dicta voluptate sapiente id vel beatae facere tempore natus architecto, eaque praesentium libero temporibus, quasi nisi laudantium ducimus, voluptas esse."
-      />
-      <Post
-        author="Kelly Eduarda"
-        content="Teste de post."
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Rafael Pablo"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur dicta voluptate sapiente id vel beatae facere tempore natus architecto, eaque praesentium libero temporibus, quasi nisi laudantium ducimus, voluptas esse."
+          />
+          <Post
+            author="Kelly Eduarda"
+            content="Teste de post."
+          />
+        </main>
+      </div>
     </>
   )
 }
