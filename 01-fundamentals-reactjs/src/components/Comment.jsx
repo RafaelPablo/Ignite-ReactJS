ea1ff9e0468e10e79ed5fa0ded1484d384ce0174
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2023-02-10 08:03:18 -0300

    cration of avatar component

--- 01-fundamentals-reactjs/src/components/Comment.jsx
+++ 01-fundamentals-reactjs/src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img className={styles.avatar} src="https://github.com/RafaelPablo.png"/> 
+            <Avatar src="https://github.com/RafaelPablo.png"/> 
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
