73e105d3512677614655227b92f6803f90e0ce8c
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2023-02-11 15:14:08 -0300

    removing errors, delete comments, lenght problem

--- 01-fundamentals-reactjs/src/components/Comment.jsx
+++ 01-fundamentals-reactjs/src/components/Comment.jsx
@@ -2,7 +2,12 @@ import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/RafaelPablo.png"/> 
@@ -15,7 +20,7 @@ export function Comment({ content }) {
                             <time title="02 de Fevereiro às 08:44" dateTime="2023-02-04 08:44:00">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24} />
                         </button>
                     </header>
