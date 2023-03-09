8a3e767147c744092a3083e4dc584c9f698a3131
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2023-03-09 07:02:15 -0300

    start of modal components

--- 03-dt-money/src/components/NewTransactionModal/index.tsx
+++ 03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,14 +1,30 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { CloseButton, Content, Overlay } from "./styles";
+import { X } from 'phosphor-react';
 
 export function NewTransactionModal() {
     return (
         <Dialog.Portal>
-            <Dialog.Overlay />
+            <Overlay />
             
-            <Dialog.Content>
+            <Content>
                 <Dialog.Title>New transaction</Dialog.Title>
-                <Dialog.Close />
-            </Dialog.Content>
+
+                <CloseButton>
+                    <X size={24}/>
+                </CloseButton>
+
+                <form action="">
+                    <input type="text" placeholder="Description" required />
+                    <input type="number" placeholder="Price" required />
+                    <input type="text" placeholder="Category" required />
+
+                    <button type="submit">
+                        Register
+                    </button>
+                </form>
+
+            </Content>
         </Dialog.Portal>
     )
 }
