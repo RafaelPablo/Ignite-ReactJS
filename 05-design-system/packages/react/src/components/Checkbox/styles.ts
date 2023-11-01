41b7aab0340883b9e69ca035aab2d1fd79d31cb3
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2023-11-01 16:42:49 -0300

    text area, text input and more

---
+++ 05-design-system/packages/react/src/components/Checkbox/styles.ts
@@ -0,0 +1,6 @@
+import * as Checkbox from '@radix-ui/react-checkbox'
+import { styled } from '../../styles'
+
+export const CheckboxContainer = styled(Checkbox.Root, {})
+
+export const CheckboxIndicator = styled(Checkbox.Indicator, {})
